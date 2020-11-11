eeb8e5a39d8ed7a65f2a550ec68b7bad793407ab
Shantanu Gontia <shantanugontia@dhcp-10-191-240-132.vpn.oracle.com>
2020-11-11 16:12:54 +0530

    Added the github link

--- src/Instructions.js
+++ src/Instructions.js
@@ -7,7 +7,7 @@ function Instructions() {
                 <li>Input any string text that you want to encode or decode.</li>
                 <li>Light weight and fast transformer.</li>
                 <li>Handy for turning encoded JavaScript URLs from complete gibberish into readable format.</li>
-                <li>Love to keep it open source. Go ahead and use the source code.</li>
+                <li>Love to keep it open source. Go ahead and use the <a href="https://github.com/shaangontia/url-encoder-decoder" target="blank">source code </a>.</li>
             </ul>
         </div>
     )
