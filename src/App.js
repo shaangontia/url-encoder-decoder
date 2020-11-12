2eadea30cbae68235fc1d29bd16a6147a1908aba
Shantanu Gontia <shantanugontia@Shantanus-MacBook-Pro.local>
2020-11-12 17:19:44 +0530

    Added footer to the tool

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Processor from './processor';
 import Instruction from './Instructions';
+import Footer from './footer';
 
 const headerText = "Powerful URL Encoder and Decoder"
 function App() {
@@ -12,6 +13,7 @@ function App() {
       </div>
       <Processor></Processor>
       <Instruction/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+function Footer() {
+    return(
+        <div className='footer'>
+            <hr />
+            <span> This is a small effort to help people to encode and decode the url in their day to day work. 
+                Please feel free to use it as per your need. There are no legal obligations involved with
+                respect to using the tool. </span>
+        </div>
+    );
+}
+
+export default Footer;
